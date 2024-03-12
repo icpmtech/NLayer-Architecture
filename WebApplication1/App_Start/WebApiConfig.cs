606012746aea898a907187fb8627c98b1347c802
Pedro Martins <pedro.mourao.martins@example.com>
2024-03-12 17:47:40 +0000

    add

--- WebApplication1/App_Start/WebApiConfig.cs
+++ WebApplication1/App_Start/WebApiConfig.cs
@@ -14,6 +14,5 @@ namespace WebApplication1.App_Start
             configuration.Routes.MapHttpRoute("API Default", "api/{controller}/{id}",
                 new { id = RouteParameter.Optional });
         }
-
     }
 }
